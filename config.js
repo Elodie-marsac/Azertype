93553530f055e3bf6a4717313cf4de7aa0bfe1fb
Elodie-marsac <126961472+Elodie-marsac@users.noreply.github.com>
2024-03-23 09:02:04 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+//Liste mots utilisés pour le jeu
+const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
+
+//Liste phrases utilisées pour le jeu
+const listePhrases = ['Pas de panique !', "La vie, l'univers et le reste", "Merci pour le poisson"]
